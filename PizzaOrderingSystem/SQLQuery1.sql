8eaa38691e33e47fd0b95aa752895d81ff83d6d2
Manju Priyaa R <mailmanjupriyaa@gmail.com>
2021-05-31 08:38:39 +0530

    SQL File

---
+++ PizzaOrderingSystem/SQLQuery1.sql
@@ -0,0 +1,20 @@
+use dbPizzaOrderSystem
+
+select * from Pizzas;
+select * from Toppings
+select * from Crusts
+select * from Orders;
+select * from Cart;
+
+alter table CustPizzas
+--Truncate table CustPizzas;
+
+--select * 
+--use dbPizza
+--use dbPizzaClient
+--select * from Customers
+--select CustomerName from Customers where CustomerId=1;
+--select PizzaName,Price from Pizzas where PizzaId=1
+
+
+SET IDENTITY_INSERT CustPizzas OFF
